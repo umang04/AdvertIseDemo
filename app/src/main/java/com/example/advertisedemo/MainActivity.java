9bbdc573d9dcc3acebe459d1e28dfa3d2cff5e36
umang04 <umang.mahendiratta@vcommission.com>
2019-11-20 12:13:53 +0530

    Learn to push code by a Demo.

---
+++ app/src/main/java/com/example/advertisedemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.advertisedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                AdvertisingIdClient.Info idInfo = null;
+                try {
+                    idInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String advertId = null;
+                try{
+                    advertId = idInfo.getId();
+                }catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+
+                return advertId;
+            }
+
+            @Override
+            protected void onPostExecute(String advertId) {
+                Toast.makeText(getApplicationContext(), advertId, Toast.LENGTH_LONG).show();
+            }
+
+        };
+        task.execute();
+
+    }
+
+}
+
+
